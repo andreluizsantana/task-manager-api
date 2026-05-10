0b4e8b0e84412697aca7a2823ba4bc9324beb6a8
Andre Luiz <andreluiz@live.com>
2026-05-10 12:22:44 -0300

    feat: configuração de paginação, batch processing e Hibernate logging, e remoção da logo do spring

--- taskhub/src/main/java/com/project/taskhub/TaskhubApplication.java
+++ taskhub/src/main/java/com/project/taskhub/TaskhubApplication.java
@@ -1,15 +1,17 @@
 package com.project.taskhub;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class TaskhubApplication {
 
-    public static void main(String[] args) {
-	SpringApplication.run(TaskhubApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplicationBuilder builder = new SpringApplicationBuilder(TaskhubApplication.class);
+    builder.bannerMode(Banner.Mode.OFF);
+    builder.run(args);
+  }
 }
